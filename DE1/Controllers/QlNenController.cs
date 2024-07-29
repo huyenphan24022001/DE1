d6a88d0751c6a84974946a6be061890a004a6de0
huyenphan24022001 <168697967+huyenphan24022001@users.noreply.github.com>
2024-07-29 11:15:14 +0700

    Add project files.

---
+++ DE1/Controllers/QlNenController.cs
@@ -0,0 +1,93 @@
+﻿using DE1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DE1.Controllers
+{
+    public class QlNenController : Controller
+    {
+        DE1Entities db = new DE1Entities();
+
+        // GET: QlNen
+        public ActionResult Index()
+        {
+            List<QLNen> list = db.QLNens.Where(x => x.IsDelete == false).ToList();
+            return View(list);
+        }
+
+        // GET: QlNen/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: QlNen/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QlNen/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QlNen/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: QlNen/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QlNen/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: QlNen/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
